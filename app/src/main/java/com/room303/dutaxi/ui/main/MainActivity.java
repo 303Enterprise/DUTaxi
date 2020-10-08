c34b0c05a8699b98dae2136a0f35009074a68795
Senex-V <vdm.snx@gmail.com>
2020-10-08 22:59:16 +0300

    Update MainActivity
    Add TAG

--- app/src/main/java/com/room303/dutaxi/ui/main/MainActivity.java
+++ app/src/main/java/com/room303/dutaxi/ui/main/MainActivity.java
@@ -1,11 +1,14 @@
 package com.room303.dutaxi.ui.main;
 
+import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import com.room303.dutaxi.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "AppDebug_MainActivity";
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
