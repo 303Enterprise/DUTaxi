f5814855a87ee86f412402e86d5e314985d5b2f7
Senex-V <vdm.snx@gmail.com>
2020-10-08 22:41:49 +0300

    Origin

---
+++ app/src/main/java/com/room303/dutaxi/ui/main/MainActivity.java
@@ -0,0 +1,16 @@
+package com.room303.dutaxi.ui.main;
+
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.room303.dutaxi.R;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
