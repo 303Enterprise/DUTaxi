3a2bf97e7a98b294cf1e536784a5e6b58324e583
Senex-V <vdm.snx@gmail.com>
2020-10-14 22:40:19 +0300

    Modified input fields

--- app/src/main/java/com/room303/dutaxi/ui/main/MainActivity.java
+++ app/src/main/java/com/room303/dutaxi/ui/main/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
 
@@ -12,26 +13,46 @@ import com.room303.dutaxi.R;
 import com.room303.dutaxi.requestitem.RequestItem;
 import com.room303.dutaxi.ui.main.createtripfragment.CreateTripFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private final RequestItem requestItem = new RequestItem();
     private static final String TAG = "AppDebug_MainActivity";
     private final CreateTripFragment createTripFragment = new CreateTripFragment();
     private FrameLayout mainContainer;
-    private FragmentManager fragmentManager;
+    private FragmentManager fragmentManager = getSupportFragmentManager();
     private FragmentTransaction fragmentTransaction;
 
     private Button leftButton;
+    private Button middleButton;
     private Button rightButton;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        fragmentManager = getSupportFragmentManager();
-        fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.add(R.id.main_container, createTripFragment);
-        fragmentTransaction.commit();
+        mainContainer = findViewById(R.id.main_container);
+        leftButton = findViewById(R.id.bottom_button_left);
+        middleButton = findViewById(R.id.bottom_button_middle);
+        rightButton = findViewById(R.id.bottom_button_right);
+        leftButton.setOnClickListener(this);
+        middleButton.setOnClickListener(this);
+        rightButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.bottom_button_left:
+                fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.main_container, createTripFragment);
+                fragmentTransaction.commitAllowingStateLoss();
+                break;
+            case R.id.bottom_button_middle:
+                break;
+            case R.id.bottom_button_right:
+                break;
+        }
     }
 }
--- app/src/main/java/com/room303/dutaxi/ui/main/createtripfragment/CreateTripFragment.java
+++ app/src/main/java/com/room303/dutaxi/ui/main/createtripfragment/CreateTripFragment.java
@@ -1,20 +1,38 @@
 package com.room303.dutaxi.ui.main.createtripfragment;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.telephony.PhoneNumberFormattingTextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import com.room303.dutaxi.R;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class CreateTripFragment extends Fragment {
 
     private static final String FIRST_DESCRIPTION = "SAMPLE DESCRIPTION";
 
-    private TextView description;
+    private EditText phoneInput;
+    private EditText vkrefInput;
+    private Button timeInputButton;
+    private RadioGroup freeseatsInput;
+
+    private int tripMinute;
+    private int tripHour;
 
     public CreateTripFragment() {
     }
@@ -23,9 +41,52 @@ public class CreateTripFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_create_trip, container, false);
-        description = rootView.findViewById(R.id.description);
-        description.setText(FIRST_DESCRIPTION);
+        phoneInput = rootView.findViewById(R.id.phone_input);
+        timeInputButton = rootView.findViewById(R.id.time_input);
+        timeInputButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cal = Calendar.getInstance();
+                tripHour = cal.get(Calendar.HOUR_OF_DAY);
+                tripMinute = cal.get(Calendar.MINUTE);
+
+                TimePickerDialog.OnTimeSetListener timeSetListener = new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        //editTextTime.setText(hourOfDay + ":" + minute );
+                        tripHour = hourOfDay;
+                        tripMinute = minute;
+                    }
+                };
+
+                TimePickerDialog timePickerDialog = new TimePickerDialog(getActivity(),
+                        timeSetListener, tripHour, tripMinute, true);
+
+                timePickerDialog.show();
+            }
+        });
+
+        freeseatsInput = rootView.findViewById(R.id.freeseats_input);
+        freeseatsInput.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case R.id.freesets_button_one:
+
+                        break;
+                    case R.id.freesets_button_two:
+
+                        break;
+                    case R.id.freesets_button_three:
+                        break;
+                }
+            }
+        });
+
 
         return rootView;
     }
+
+
 }
