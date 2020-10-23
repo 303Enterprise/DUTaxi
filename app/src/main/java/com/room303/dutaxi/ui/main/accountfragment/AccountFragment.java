5e0097960fa1b0133b17d91139d64b213caacad2
Senex-V <vdm.snx@gmail.com>
2020-10-23 11:09:40 +0300

    AccountFragment basic layout done

---
+++ app/src/main/java/com/room303/dutaxi/ui/main/accountfragment/AccountFragment.java
@@ -0,0 +1,27 @@
+package com.room303.dutaxi.ui.main.accountfragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.room303.dutaxi.R;
+
+public class AccountFragment extends Fragment {
+
+    public AccountFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_account, container, false);
+        return rootView;
+    }
+
+    private void initUi() {
+
+    }
+}
