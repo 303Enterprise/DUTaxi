ae30d6573fb1adcf7fe48695cb6688c4a6f02e4a
V-Senex <vdm.snx@gmail.com>
2020-11-03 19:10:08 +0300

    EditFragment basic logic done

--- app/src/main/java/com/room303/dutaxi/ui/main/accountfragment/editfragment/EditFragment.java
+++ app/src/main/java/com/room303/dutaxi/ui/main/accountfragment/editfragment/EditFragment.java
@@ -1,13 +1,16 @@
 package com.room303.dutaxi.ui.main.accountfragment.editfragment;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,11 +18,14 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.room303.dutaxi.R;
 
 public class EditFragment extends Fragment implements View.OnClickListener {
     private NavController navController;
+    private BottomNavigationView bottomNavigationView;
 
+    private TextView toolbarTopic;
     private EditText input;
     private TextView description;
     private ImageButton cancel;
@@ -29,11 +35,7 @@ public class EditFragment extends Fragment implements View.OnClickListener {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_edit, container, false);
-        cancel = rootView.findViewById(R.id.toolbar_button_cancel);
-        commit = rootView.findViewById(R.id.toolbar_button_commit);
-
-        input = rootView.findViewById(R.id.edit_input);
-        description = rootView.findViewById(R.id.edit_description);
+        initUi(rootView);
         return rootView;
     }
 
@@ -44,12 +46,40 @@ public class EditFragment extends Fragment implements View.OnClickListener {
     }
 
     @Override
+    public void onResume() {
+        super.onResume();
+        bottomNavigationView.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        bottomNavigationView.setVisibility(View.VISIBLE);
+    }
+
+    @Override
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.toolbar_button_cancel:
+                navController.navigate(R.id.action_editFragment_to_accountFragment);
                 break;
             case R.id.toolbar_button_commit:
+                navController.navigate(R.id.action_editFragment_to_accountFragment);
                 break;
         }
+    }
+
+    private void initUi(View rootView) {
+        toolbarTopic = rootView.findViewById(R.id.toolbar_text_title);
+        cancel = rootView.findViewById(R.id.toolbar_button_cancel);
+        cancel.setOnClickListener(this);
+        commit = rootView.findViewById(R.id.toolbar_button_commit);
+        commit.setOnClickListener(this);
+        input = rootView.findViewById(R.id.edit_input);
+        input.requestFocus();
+        ((InputMethodManager)getActivity().getSystemService(Context.INPUT_METHOD_SERVICE))
+                .toggleSoftInput(0, 0);
+        description = rootView.findViewById(R.id.edit_description);
+        bottomNavigationView = getActivity().findViewById(R.id.bottomNavigationView);
     }
 }
